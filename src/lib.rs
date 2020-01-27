38af04e83926b8c8bdbea02e675269996a626f98
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2020-01-27 18:35:32 +0330

    improvement

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 #[cfg(any(target_os = "linux", target_os = "window"))]
 pub type Arg = ();
-#[cfg(target_os = "android")]
-pub type Arg = &mut i32;
 
 #[cfg(any(target_os = "linux", target_os = "window"))]
 #[macro_export]
@@ -14,13 +12,19 @@ macro_rules! main {
 }
 
 #[cfg(target_os = "android")]
+pub extern crate rust_graphics_android as android;
+
+#[cfg(target_os = "android")]
+pub type Arg = &'static mut android::glue::AndroidApp;
+
+#[cfg(target_os = "android")]
 #[macro_export]
 macro_rules! main {
     ($main_function:ident) => {
         #[allow(dead_code)]
         #[no_mangle]
-        extern "C" fn android_main(android_app: *mut std::os::raw::c_void) {
-            $main_function();
+        extern "C" fn android_main(android_app: &'static mut $crate::android::glue::AndroidApp) {
+            $main_function(android_app);
         }
     };
 }
